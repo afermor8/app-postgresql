1d701ed15c61448972ea6e3d21405c043c451406
afermor8 <ara.fer.mor@gmail.com>
2022-10-30 21:06:08 +0100

    He corregido errores varios del código de la aplicación y del formulario

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import psycopg2
 
 app = Flask (__name__)
 
-def con_db():
+def get_db_conn():
     conn = None
     peli = None
     actor = None
@@ -14,43 +14,44 @@ def con_db():
         conn = psycopg2.connect(host="localhost", dbname="maravilla", user="admin", password="admin")
     except Exception as e:
         print("No puedo conectar a la base de datos:",e)
-#    finally:
-#        cursor.close()
-#        conn.close()
 
-#conn = psycopg2.connect(
-#    host="localhost",
-#    database="maravilla",
-#    user=os.environ['admin'],
-#    password=os.environ['admin'])
+    return conn
 
 
 def marvels():
-    peli = conn.peli()
+    conn = get_db_conn()
+
+    peli = conn.cursor()
     peli.execute("select * from pelicula;")
     peliculas = peli.fetchall()
-    tabla = conn.tabla()
-    tablas=tabla.execute("\dt")
-    actor = conn.actor()
+
+    tabla_cursor = conn.cursor()
+    tabla_cursor.execute("""SELECT table_name FROM information_schema.tables
+       WHERE table_schema = 'public'""")
+    tablas = tabla_cursor.fetchall()
+
+    actor = conn.cursor()
     actor.execute("select * from actor;")
     actors=actor.fetchall()
+
     return render_template("maravilla.html", tablas=tablas, peliculas=peliculas, actors=actors)
 
 
-@app.route('/',methods=["GET","POST"])
+@app.route('/',methods=["GET"])
 def index():
     if not session.get("logged_in"):
         return render_template("index.html")
     else:
         return marvels()
 
-@app.route('/login', methods=['GET', 'POST'])
-def login_required():
+@app.route('/login', methods=["POST"])
+def login():
     if request.form['password'] == 'admin' and request.form['username'] == 'admin':
         session['logged_in'] = True
     return redirect("/")
 
 if __name__ == "__main__":
-#    app.secret_key = os.urandom(12)
+    app.secret_key = os.urandom(12)
+    app.config['SESSION_TYPE'] = 'filesystem'
 
     app.run('0.0.0.0' ,debug=False)
